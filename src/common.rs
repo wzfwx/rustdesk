0fe0f6585592a8cae52aac830c9bbbac48783186
wzfwx <wzfwx@qq.com>
2025-09-07 00:03:50 +0800

    更新配置文件

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://rustdesk.rs.com:11443".to_owned()
 }
 
 #[inline]
